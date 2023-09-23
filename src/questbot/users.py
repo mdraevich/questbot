a0dbef7531d399865ec35527d0f6bf46aba41659
Matthew <draevich.matvey@gmail.com>
2023-09-24 02:22:34 +0300

    implement User and UserState classes

---
+++ src/questbot/users.py
@@ -0,0 +1,47 @@
+import logging
+from enum import Enum
+
+
+logger = logging.getLogger(__name__)
+
+
+class UserState(Enum):
+    IDLE = 1
+    REGISTERED = 2
+    PLAYING = 3
+    DELETED = 4
+
+
+class User():
+    """
+    represents a general user account
+    """
+
+    def __init__(self, user_id):
+        self._name = ""
+        self._user_id = user_id
+        self.state = UserState.IDLE
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def user_id(self):
+        return self._user_id
+
+    @property
+    def state(self):
+        return self._state
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("name must be string")
+        self._name = value
+
+    @state.setter
+    def state(self, value):
+        if not isinstance(value, UserState):
+            raise ValueError("state must be an instance of UserState")
+        self._state = value
