6d275bfe2294a7c7123df3aabad117ad36d5f7de
Matthew <draevich.matvey@gmail.com>
2023-10-08 01:47:50 +0300

    implement teamcontroller class structure; add new arg qevent in QuestController.process_change; register TeamController instances during quest_definition registration

--- src/questbot/controllers.py
+++ src/questbot/controllers.py
@@ -4,6 +4,7 @@ import threading
 from datetime import datetime, timedelta
 
 from questbot.events import QuestEvent, EventState, EventDistributor
+from questbot.definitions import QuestDefinition, TeamDefinition
 
 logger = logging.getLogger(__name__)
 
@@ -37,13 +38,24 @@ class QuestController():
         returns True if newly registered
         """
 
+        if not isinstance(quest_definition, QuestDefinition):
+            raise ValueError("quest_definition must be an instance of "
+                             "QuestDefinition class")
+
         if quest_definition.name in self._quests:
             return False
 
-        self._quests[quest_definition.name] = QuestEvent(quest_definition)
+        qevent = QuestEvent(quest_definition)
+        for team_definition in quest_definition.get_teams():
+            qevent.register_team_controller(TeamController(team_definition))
+            logger.debug(f"Registered a new team controller with "
+                         f"team_definition.name='{team_definition.name}' "
+                         f"for qevent.quest.name='{qevent.quest.name}'")
+
+        self._quests[quest_definition.name] = qevent
         return True
 
-    def process_change(self, newstate):
+    def process_change(self, qevent, newstate):
         if newstate == EventState.SCHEDULED:
             self.distributor.notify("Quest is scheduled now, sign it off now!")
         elif newstate == EventState.RUNNING:
@@ -76,7 +88,7 @@ class QuestController():
                 if curstate != newstate:
                     logger.info(f"quest event ['{qevent.quest.name}'] changed "
                                 f"its state: {curstate.name} => {newstate.name}")
-                    self.process_change(newstate)
+                    self.process_change(qevent, newstate)
                 qevent.state = newstate
 
             time.sleep(self.UPDATER_INTERVAL)
@@ -88,9 +100,35 @@ class QuestController():
 class TeamController():
     """
     responsible for interacting with a team
-    in accordance to quest definition
-    responsible for event distribution within the team as well
+    in accordance to team definition
     """
 
-    def __init__(self):
+    def __init__(self, team_definition):
+        self.team = team_definition
+        self._distributor = EventDistributor()
+
+    @property
+    def team(self):
+        return self._team
+
+    @team.setter
+    def team(self, value):
+        if not isinstance(value, TeamDefinition):
+            raise ValueError("value must be an instance of "
+                             "TeamDefinition class")
+        self._team = value
+
+    @property
+    def distributor(self):
+        return self._distributor
+
+    def give_hint(self):
+        pass
+
+    def check_answer(self, value):
+        """
+        return True if answer is right
+        return False if answer is wrong
+        """
+
         pass
